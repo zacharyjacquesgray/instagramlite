76e7551d82f032b89b2431774ced6ce19232128f
Zachary Gray <zgra6187@uni.sydney.edu.au>
2023-06-23 18:07:27 +1000

    Image loads from server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const app = express();
 const axios = require('axios');
 const cors = require('cors');
+const fs = require('fs');
 
+const app = express();
 const port = 3000;
 
 app.use(cors());
@@ -10,14 +11,42 @@ app.use(cors());
 app.get('/:username', async (req, res) => {
   const { username } = req.params;
 
+  const baseUrl = 'http://localhost:3000'; // Update with your server's URL
+  const imageUrl = `${baseUrl}/${username}/image`;
+
   try {
     const response = await axios.get(`https://www.instagram.com/${username}/?__a=1&__d=dis`);
     const data = response.data;
-    res.json(data);
+
+    // Prepare the response object
+    const responseObject = {
+      imageUrl: imageUrl,
+      data: data,
+    };
+
+    res.json(responseObject);
   } catch (error) {
     console.error('Error:', error);
     res.status(500).send('Internal Server Error');
   }
+});
+
+app.get('/:username/image', (req, res) => {
+  const imageUrl = 'https://instagram.fsyd3-1.fna.fbcdn.net/v/t51.2885-15/355228759_776809164177900_9199623976963649295_n.jpg?stp=dst-jpg_e35_p1080x1080&_nc_ht=instagram.fsyd3-1.fna.fbcdn.net&_nc_cat=1&_nc_ohc=7tDIStHkRMgAX84RVH5&edm=AKEQFekBAAAA&ccb=7-5&oh=00_AfDkTP2rGdud3HqDLu2diIvKMcLNKmD8dLMGEty0lhSwBA&oe=649B21F2&_nc_sid=1349e3';
+
+  // Proxy the image request to the external URL
+  axios({
+    method: 'get',
+    url: imageUrl,
+    responseType: 'stream',
+  })
+    .then((response) => {
+      response.data.pipe(res);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      res.status(500).send('Internal Server Error');
+    });
 });
 
 app.listen(port, () => {
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -2,20 +2,21 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ImageParser from './ImageParser';
 
-// Information to reach API
-const baseUrl = 'http://localhost:3000'; // Update with your server's URL
+const baseUrl = 'http://localhost:3000'; // Server URL
 const endpoint = baseUrl + '/';
-const mediaJson = ''
+const mediaJson = '';
 
 const SearchResults = ({ username }) => {
   const [displayData, setDisplayData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [imageUrl, setImageUrl] = useState('');
 
   useEffect(() => {
     const getResults = async () => {
       try {
         const response = await axios.get(`${endpoint}${username}`);
         setDisplayData(response.data);
+        setImageUrl(`${baseUrl}/${username}/image`); // Set the imageUrl from the response data
       } catch (error) {
         console.log(error);
       } finally {
@@ -30,13 +31,20 @@ const SearchResults = ({ username }) => {
     return <p>Loading {username}...</p>;
   }
 
-  //return <p>{JSON.stringify(displayData, null, 2)}</p>;
+  if (!displayData || !displayData.data || !displayData.data.graphql || !displayData.data.graphql.user) {
+    return <p>No data available</p>;
+  }
+
   return (
     <div>
       <ImageParser json={JSON.stringify(displayData)} />
-      <h3>{displayData.graphql.user.edge_owner_to_timeline_media.edges[0].node.display_url}</h3>
-      <h3>{displayData.graphql.user.edge_owner_to_timeline_media.edges[1].node.display_url}</h3>
-      <h3>{displayData.graphql.user.edge_owner_to_timeline_media.edges[2].node.display_url}</h3>
+
+      <img src={imageUrl} alt="Instagram Image" />
+
+      <h3>{displayData.data.graphql.user.edge_owner_to_timeline_media.edges[0].node.display_url}</h3>
+      <h3>{displayData.data.graphql.user.edge_owner_to_timeline_media.edges[1].node.display_url}</h3>
+      <h3>{displayData.data.graphql.user.edge_owner_to_timeline_media.edges[2].node.display_url}</h3>
+      <h4>{imageUrl}</h4>
       <pre>{JSON.stringify(displayData, null, 2)}</pre>
     </div>
   );
